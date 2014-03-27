41476d235b7712fcc97cf01afdc43012d1da227a
Michael de Jong <michaelj@minicom.nl>
2014-03-27 21:02:01 +0100

    Give requesting user read-write permission on requested repository.

--- src/main/java/nl/tudelft/ewi/devhub/server/backend/ProjectsBackend.java
+++ src/main/java/nl/tudelft/ewi/devhub/server/backend/ProjectsBackend.java
@@ -14,20 +14,22 @@ import nl.tudelft.ewi.devhub.server.database.entities.User;
 import nl.tudelft.ewi.devhub.server.web.errors.ApiError;
 import nl.tudelft.ewi.git.client.GitServerClient;
 import nl.tudelft.ewi.git.models.RepositoryModel;
+import nl.tudelft.ewi.git.models.RepositoryModel.Level;
 import nl.tudelft.ewi.git.models.UserModel;
 
 import org.hibernate.exception.ConstraintViolationException;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 
 @Slf4j
 public class ProjectsBackend {
-	
+
 	private static final int USER_ID = 1;
-	
-	private static final String ALREADY_REGISTERED_FOR_COURSE = "already-registered-for-course"; 
+
+	private static final String ALREADY_REGISTERED_FOR_COURSE = "already-registered-for-course";
 	private static final String COULD_NOT_FIND_COURSE = "could-not-find-course";
 	private static final String COULD_NOT_CREATE_GROUP = "could-not-create-group";
 
@@ -37,7 +39,7 @@ public class ProjectsBackend {
 	private final Users users;
 
 	private final Object groupNumberLock = new Object();
-	
+
 	@Inject
 	ProjectsBackend(GroupMemberships groupMemberships, Groups groups, Users users, GitServerClient client) {
 		this.groupMemberships = groupMemberships;
@@ -52,27 +54,27 @@ public class ProjectsBackend {
 		if (course == null) {
 			throw new ApiError(COULD_NOT_FIND_COURSE);
 		}
-		
+
 		synchronized (groupNumberLock) {
-			
+
 			List<Group> courseGroups = groups.find(course);
 			Set<Long> groupNumbers = getGroupNumbers(courseGroups);
-			
+
 			// Ensure that requester has no other projects for same course.
 			if (isAlreadyRegisteredForCourse(requester, courseGroups)) {
 				throw new ApiError(ALREADY_REGISTERED_FOR_COURSE);
 			}
-			
+
 			// Select first free group number.
 			long newGroupNumber = 1;
 			while (groupNumbers.contains(newGroupNumber)) {
 				newGroupNumber++;
 			}
-			
+
 			Group group = new Group();
 			group.setCourse(course);
 			group.setGroupNumber(newGroupNumber);
-			
+
 			boolean worked = false;
 			for (int attempt = 1; attempt <= 3 && !worked; attempt++) {
 				try {
@@ -84,22 +86,26 @@ public class ProjectsBackend {
 					group.setGroupNumber(group.getGroupNumber() + 1);
 				}
 			}
-			
+
 			if (!worked) {
 				throw new ApiError(COULD_NOT_CREATE_GROUP);
 			}
-			
+
 			GroupMembership membership = new GroupMembership();
 			membership.setGroup(group);
 			membership.setUser(requester);
 			groupMemberships.persist(membership);
-			
+
 			UserModel userModel = new UserModel();
 			userModel.setName(requester.getNetId());
 			client.users().ensureExists(userModel);
-			
+
 			RepositoryModel repoModel = new RepositoryModel();
 			repoModel.setName(group.getRepoName());
+			repoModel.setPermissions(ImmutableMap.<String, Level> builder()
+					.put(userModel.getName(), Level.READ_WRITE)
+					.build());
+
 			client.repositories().create(repoModel);
 		}
 	}
@@ -115,7 +121,7 @@ public class ProjectsBackend {
 		}
 		return false;
 	}
-	
+
 	private Set<Long> getGroupNumbers(List<Group> groups) {
 		Set<Long> groupNumbers = Sets.newHashSet();
 		for (Group group : groups) {

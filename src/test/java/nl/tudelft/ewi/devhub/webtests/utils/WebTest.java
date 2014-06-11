c4dffc9fef815eea9f4e5d2c0c175fd9a3534878
jwgmeligmeyling <j.gmeligmeyling@student.tudelft.nl>
2014-06-11 18:51:54 +0200

    Screenshot testwatcher

--- src/test/java/nl/tudelft/ewi/devhub/webtests/utils/WebTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/utils/WebTest.java
@@ -1,5 +1,10 @@
 package nl.tudelft.ewi.devhub.webtests.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
 import com.google.inject.AbstractModule;
 
 import nl.tudelft.ewi.build.client.BuildServerBackend;
@@ -12,15 +17,20 @@ import nl.tudelft.ewi.devhub.webtests.views.LoginView;
 import nl.tudelft.ewi.git.client.GitServerClient;
 import nl.tudelft.ewi.git.client.GitServerClientMock;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public abstract class WebTest {
-	
+
 	public static final String NET_ID = "student1";
 	public static final String PASSWORD = "student1";
 
@@ -72,5 +82,33 @@ public abstract class WebTest {
 	public static void afterClass() throws Exception {
 		server.stopServer();
 	}
+	
+	@Rule
+	public TestWatcher watchman = new TestWatcher() {
+
+		@Override
+		protected void failed(Throwable e, Description description) {
+			try {
+				long now = System.currentTimeMillis();
+				createScreenshot(e, description, now);
+				writeStacktrace(e, description, now);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		private void createScreenshot(Throwable e, Description description, long now) throws IOException {
+			File input = ((FirefoxDriver) driver).getScreenshotAs(OutputType.FILE);
+			String filename = String.format("Screenshot-%s-%s.jpg", now, description.getMethodName());
+			File output = new File(filename);
+			FileUtils.copyFile(input, output);			
+		}
+		
+		private void writeStacktrace(Throwable e, Description description, long now) throws FileNotFoundException {
+			String filename = String.format("Stacktrace-%s-%s.txt", now, description.getMethodName());
+			e.printStackTrace(new PrintStream(filename));
+		}
+		
+	};
 	
 }

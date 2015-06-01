dcbd44ea650df676847b6aff15ce7f7a33370c82
LiamClark <L.A.Clark@student.tudelft.nl>
2015-06-01 14:59:13 +0200

    restructured the algorithm, to walk the directory instead of tracking the diff

--- src/main/java/nl/tudelft/ewi/devhub/server/backend/warnings/IgnoredFileWarningGenerator.java
+++ src/main/java/nl/tudelft/ewi/devhub/server/backend/warnings/IgnoredFileWarningGenerator.java
@@ -1,45 +1,69 @@
 package nl.tudelft.ewi.devhub.server.backend.warnings;
 
+import com.google.common.collect.Sets;
+import com.google.inject.servlet.RequestScoped;
 import lombok.SneakyThrows;
 import nl.tudelft.ewi.devhub.server.database.entities.Commit;
-import nl.tudelft.ewi.devhub.server.database.entities.warnings.CommitWarning;
-import nl.tudelft.ewi.devhub.server.database.entities.warnings.CommitedIgnoredFiles;
 import nl.tudelft.ewi.devhub.server.database.entities.warnings.IgnoredFileWarning;
 import nl.tudelft.ewi.devhub.server.web.models.GitPush;
-import nl.tudelft.ewi.git.models.DiffModel;
+import nl.tudelft.ewi.git.client.Repository;
+import nl.tudelft.ewi.git.models.EntryType;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Created by LC on 30/05/15.
  */
-public class IgnoredFileWarningGenerator extends GitCommitPushWarningGenerator<CommitedIgnoredFiles> {
+@RequestScoped
+public class IgnoredFileWarningGenerator extends GitCommitPushWarningGenerator<IgnoredFileWarning> {
 
     private static final String[] extensions = {".class", ".bin.pdf", ".doc", ".docx", ".jar"};
-    private static final String[] folders = {".iml", ".idea/", "", ".metadata/", ".settings/", ".project/", ".classpath/", "target/", "bin/", ".metadata/"};
-
+    private static final String[] folders = {".iml", ".idea/", "", ".metadata/", ".settings/",
+            ".project/", ".classpath/", "target/", "bin/", ".metadata/"};
+    private Repository repository;
 
     @SneakyThrows
     @Override
-    public Collection<CommitedIgnoredFiles> generateWarnings(Commit commit, GitPush attachment) {
-        nl.tudelft.ewi.git.client.Commit gitCommit = getGitCommit(commit,attachment);
-        List<DiffModel.DiffFile> files = gitCommit.diff().getDiffs();
-        files.stream().map(DiffModel.DiffFile::getNewPath).forEach(s -> this.checkFile(s));
-        return null;
+    public Collection<IgnoredFileWarning> generateWarnings(Commit commit, GitPush attachment) {
+        repository = getRepository(commit);
+        Set<IgnoredFileWarning> warnings = Sets.newHashSet();
+        walkCommitStructure("",commit.getCommitId(),warnings);
+        return warnings;
+    }
+    @SneakyThrows
+    public void walkCommitStructure(String path, String commit, Set<IgnoredFileWarning> warnings){
+
+        if(folderViolation(path)){
+            IgnoredFileWarning warning = new IgnoredFileWarning();
+            warning.setFileName(path);
+            warnings.add(warning);
+        }
+
+        Map<String,EntryType> directory = repository.listDirectoryEntries(commit,path);
+        directory.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(EntryType.FOLDER))
+                .forEach(entry-> walkCommitStructure(path+entry.getKey(),commit,warnings));
+
+        directory.entrySet().stream()
+                .filter(entry -> !entry.getValue().equals(EntryType.FOLDER))
+                .map(Map.Entry::getKey)
+                .filter(IgnoredFileWarningGenerator::fileViolation)
+                .map(file -> {
+                    IgnoredFileWarning warning = new IgnoredFileWarning();
+                    warning.setFileName(file);
+                    return warning;
+                })
+                .forEach(warnings::add);
     }
 
+    private static boolean fileViolation(String path){
+       return Stream.of(extensions).filter(path::endsWith).findAny().isPresent();
+    }
 
-    private static List<CommitWarning> checkFile(String path){
-        List<CommitWarning> warnings  = new ArrayList<>();
-        for(String extension : extensions) {
-            if(path.endsWith(extension)) {
-                IgnoredFileWarning ignoredFileWarning = new IgnoredFileWarning();
-                String filename = path.substring(path.lastIndexOf('/' + 1));
-                ignoredFileWarning.setFileName(filename);
-            }
-        }
-        return warnings;
+    private static boolean folderViolation(String path){
+        return Stream.of(folders).filter(path::endsWith).findAny().isPresent();
     }
 }

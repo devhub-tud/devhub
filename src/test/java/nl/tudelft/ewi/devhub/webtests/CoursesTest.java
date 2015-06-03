65fbead26eb01e5203c7f007e2ee989d782af2bc
Austaon <aurelbansagi@gmail.com>
2015-06-03 16:23:29 +0200

    Added assignment methods for course view

--- src/test/java/nl/tudelft/ewi/devhub/webtests/CoursesTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/CoursesTest.java
@@ -1,10 +1,16 @@
 package nl.tudelft.ewi.devhub.webtests;
 
+import com.google.inject.matcher.Matcher;
 import nl.tudelft.ewi.devhub.webtests.utils.WebTest;
 
 import nl.tudelft.ewi.devhub.webtests.views.CourseView;
+import nl.tudelft.ewi.devhub.webtests.views.CourseView.Assignment;
 import nl.tudelft.ewi.devhub.webtests.views.CourseView.Group;
 import nl.tudelft.ewi.devhub.webtests.views.CoursesView;
+import nl.tudelft.ewi.devhub.webtests.views.CoursesView.CourseOverview;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
 
 import java.util.List;
@@ -17,7 +23,8 @@ public class CoursesTest extends WebTest {
 	public static final String ASSISTANT_NET_ID = "assistant1";
 	public static final String ASSISTANT_PASSWORD = "assistant1";
 	public static final String PROJECT_NAME = "TI1705 - Softwarekwaliteit & Testen";
-	
+	public static final String CHANGED_ASSIGNMENT_NAME = "Changed Assignment hype!";
+
 	/**
 	 * <h1>Opening the projects page.</h1>
 	 * 
@@ -97,6 +104,45 @@ public class CoursesTest extends WebTest {
 			.toCoursesView()
 			.listAssistingCourses().get(0).click()
 			.listGroups().get(0).click();
+	}
+
+	@Test
+	public void testAssistentEditAssignment() {
+		CourseView overview = openLoginScreen()
+			.login(ASSISTANT_NET_ID, ASSISTANT_PASSWORD)
+			.toCoursesView()
+			.listAssistingCourses().get(0).click();
+
+		CourseView.Assignment assignment = overview.listAssignments().get(0);
+
+		assignment.edit(
+			assignment.getDueDate(),
+			CHANGED_ASSIGNMENT_NAME,
+			"1234567890"
+		);
+
+		assertThat(
+			overview.listAssignments(),
+			hasItem(
+				hasName("Changed Assignment hype!")
+			)
+		);
+	}
+
+	private static BaseMatcher<Assignment> hasName(final String name) {
+		return new TypeSafeMatcher<Assignment>() {
+
+			@Override
+			protected boolean matchesSafely(Assignment assignment) {
+				return assignment.getName().equals(name);
+			}
+
+			@Override
+			public void describeTo(Description description) {
+				description.appendText("Name should be ").appendText(name);
+			}
+
+		};
 	}
 
 }
--- src/test/java/nl/tudelft/ewi/devhub/webtests/views/CourseView.java
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/views/CourseView.java
@@ -2,12 +2,16 @@ package nl.tudelft.ewi.devhub.webtests.views;
 
 import static org.junit.Assert.assertEquals;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
 
+import nl.tudelft.ewi.devhub.server.database.entities.Course;
 import nl.tudelft.ewi.devhub.webtests.utils.Dom;
 
 import org.openqa.selenium.By;
@@ -31,19 +35,51 @@ public class CourseView extends AuthenticatedView {
 		WebElement groupTable = getDriver().findElements(TABLE_XPATH).get(0);
 		assertEquals(Dom.prevSibling(groupTable).getText(), "Groups");
 
-		WebElement Assignmenttable = getDriver().findElements(TABLE_XPATH).get(1);
+		WebElement Assignmenttable = getAssignmentTable();
 		assertEquals(Dom.prevSibling(Assignmenttable).getText(), "Assignments");
 	}
-
+	private WebElement getAssignmentTable() {
+		return getDriver().findElements(TABLE_XPATH).get(1);
+	}
+	
+	/**
+	 * @return A list of visible groups
+	 */
 	public List<Group> listGroups() {
 		invariant();
 		WebElement myGroupTable = getDriver().findElements(TABLE_XPATH).get(0);
 		return listGroupsInTable(myGroupTable);
 	}
 	
+	/**
+	 * @return A list of visible assignments
+	 */
 	public List<Assignment> listAssignments() {
 		invariant();
-		return null;
+		WebElement assignmentTable = getAssignmentTable();
+		return listAssignmentsInTable(assignmentTable);
+	}
+	
+	/**
+	 * Adds an assignment
+	 * @param dueDate		deadline of the assignment
+	 * @param name			name of the assignment
+	 * @param description	a summary of the assignment
+	 */
+	public void addAssignment(Date dueDate, String name, String description) {
+		invariant();
+		WebElement assignmentTable = getAssignmentTable();
+		assignmentTable.findElement(By.xpath("..")).findElement(By.className("btn")).click();
+		
+		SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+
+		getDriver().findElement(By.id("due-date")).sendKeys(ft.format(dueDate));		
+		getDriver().findElement(By.id("name")).sendKeys(name);
+		getDriver().findElement(By.id("summary")).sendKeys(description);
+
+		getDriver().findElement(By.className("col-sm-offset-2")).findElement(By.className("btn-primary")).click();
+		
+		invariant();
 	}
 	
 	private List<Group> listGroupsInTable(WebElement table) {
@@ -62,6 +98,39 @@ public class CourseView extends AuthenticatedView {
 		}
 		return groups;
 	}
+
+	private List<Assignment> listAssignmentsInTable(WebElement table) {
+		List<WebElement> entries = table.findElements(By.tagName("tr"));
+		if (entries.size() == 1) {
+			if (!Dom.isVisible(entries.get(0), By.tagName("a"))) {
+				return Lists.newArrayList();
+			}
+		}
+
+		SimpleDateFormat ft = new SimpleDateFormat("EE, dd LL yyyy HH:mm");
+		
+		List<Assignment> assignments = Lists.newArrayList();
+		for (WebElement entry : entries) {
+			List<WebElement> assignmentLinks = entry.findElements(By.tagName("td"));
+			if(assignmentLinks.isEmpty())
+				continue;
+			Date dueDate;
+			try {
+				dueDate = ft.parse(assignmentLinks.get(2).getText());
+			} catch (ParseException e) {
+				dueDate = new Date(0);
+			}
+
+			Assignment assignment = new Assignment(
+					Integer.parseInt(assignmentLinks.get(0).getText()), 
+					assignmentLinks.get(1).getText(),
+					dueDate,
+					assignmentLinks.get(2).findElement(By.className("btn")));
+
+			assignments.add(assignment);
+		}
+		return assignments;
+	}
 	
 	@Data
 	public class Group {
@@ -78,7 +147,9 @@ public class CourseView extends AuthenticatedView {
 
 	@Data
 	public class Assignment {
+		private final int number;
 		private final String name;
+		private final Date dueDate;
 
 		@Getter(AccessLevel.NONE)
 		private final WebElement anchor;
@@ -87,6 +158,43 @@ public class CourseView extends AuthenticatedView {
 			anchor.click();
 			return new AuthenticatedView(getDriver());
 		}
+
+		/**
+		 * Shortcut method to edit an assignment.
+		 *
+		 * @param dueDate    	deadline 	(leave null for no change)
+		 * @param name			name		(leave null for no change)
+		 * @param description	description (leave null for no change)
+		 */
+		public CourseView edit(Date dueDate, String name, String description) {
+			click();
+
+			if(dueDate != null)
+			{
+				SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+				WebElement dateEle = getDriver().findElement(By.id("due-date"));
+				dateEle.clear();
+				dateEle.sendKeys(ft.format(dueDate));
+			}
+			if(name != null)
+			{
+				WebElement nameEle = getDriver().findElement(By.id("name"));
+				nameEle.clear();
+				nameEle.sendKeys(name);
+			}
+			if(description != null)
+			{
+				WebElement descEle = getDriver().findElement(By.id("summary"));
+				descEle.clear();
+				descEle.sendKeys(description);
+			}
+
+			getDriver().findElement(By.className("col-sm-offset-2")).findElement(By.className("btn-primary")).click();
+
+			invariant();
+			return CourseView.this;
+		}
+
 	}
 
 }

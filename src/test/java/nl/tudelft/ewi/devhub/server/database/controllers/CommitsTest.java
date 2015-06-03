78bbe99f5ea5fc483311344a8f3e7e25e76d126b
Jan-Willem Gmelig Meyling <j.gmeligmeyling@student.tudelft.nl>
2015-06-03 11:34:53 +0200

    Test passing now

--- src/test/java/nl/tudelft/ewi/devhub/server/database/controllers/CommitsTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/server/database/controllers/CommitsTest.java
@@ -1,28 +1,49 @@
 package nl.tudelft.ewi.devhub.server.database.controllers;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
-
-import javax.inject.Inject;
-
+import com.google.inject.AbstractModule;
+import lombok.SneakyThrows;
 import nl.tudelft.ewi.devhub.server.database.entities.Commit;
 import nl.tudelft.ewi.devhub.server.database.entities.CommitComment;
 import nl.tudelft.ewi.devhub.server.database.entities.Course;
 import nl.tudelft.ewi.devhub.server.database.entities.Group;
 import nl.tudelft.ewi.devhub.server.database.entities.User;
-
+import nl.tudelft.ewi.git.client.Repositories;
+import nl.tudelft.ewi.git.client.Repository;
 import org.jukito.JukitoRunner;
 import org.jukito.UseModules;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(JukitoRunner.class)
-@UseModules(TestDatabaseModule.class)
+@UseModules({TestDatabaseModule.class, CommitsTest.CommitsTestModule.class})
 public class CommitsTest {
+
+	public static class CommitsTestModule extends AbstractModule {
+
+		@Override
+		@SneakyThrows
+		protected void configure() {
+			Repositories repositories = Mockito.mock(Repositories.class);
+			Repository repository = Mockito.mock(Repository.class);
+			nl.tudelft.ewi.git.client.Commit commit = Mockito.mock(nl.tudelft.ewi.git.client.Commit.class);
+
+			bind(Repositories.class).toInstance(repositories);
+			Mockito.when(repositories.retrieve(Mockito.anyString())).thenReturn(repository);
+			Mockito.when(repository.retrieveCommit(Mockito.anyString())).thenReturn(commit);
+			Mockito.when(commit.getParents()).thenReturn(new String[] {});
+		}
+
+	}
 	
 	@Inject
 	private Random random;
@@ -32,13 +53,17 @@ public class CommitsTest {
 	
 	@Inject
 	private Courses courses;
-	
-	@Inject
-	private Commits commits;
-	
+
 	@Inject
 	private Users users;
-	
+
+	@Inject
+	private EntityManager entityManager;
+
+	@Inject
+	private Repositories repositories;
+	@Inject
+	private Commits commits;
 	
 	@Test
 	public void testEnsureCommitInRepository() {

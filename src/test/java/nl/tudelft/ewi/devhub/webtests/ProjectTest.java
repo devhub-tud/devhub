a7cf9532c4fddd4cee39588747a4b4d0127eb8f3
ardwennem2 <tom_brunner@hotmail.com>
2015-06-10 16:04:30 +0200

    Added assignments overview view and contributors test

--- src/test/java/nl/tudelft/ewi/devhub/webtests/ProjectTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/ProjectTest.java
@@ -8,6 +8,8 @@ import nl.tudelft.ewi.devhub.server.database.entities.User;
 import nl.tudelft.ewi.devhub.webtests.utils.WebTest;
 import nl.tudelft.ewi.devhub.webtests.views.CommitsView;
 import nl.tudelft.ewi.devhub.webtests.views.CommitsView.Commit;
+import nl.tudelft.ewi.devhub.webtests.views.ContributorsView;
+import nl.tudelft.ewi.devhub.webtests.views.ContributorsView.Contributor;
 import nl.tudelft.ewi.devhub.webtests.views.DiffInCommitView;
 import nl.tudelft.ewi.devhub.webtests.views.DiffInCommitView.DiffElement;
 import nl.tudelft.ewi.git.models.CommitModel;
@@ -168,6 +170,33 @@ public class ProjectTest extends WebTest {
 		List<DiffElement> list = view.listDiffs();
 		DiffElement result = list.get(0);
 		result.assertEqualTo(diffBlameModel.getDiffs().get(0));
+	}
+
+	@Test
+	public void testListContributors() {
+		ContributorsView view = openLoginScreen()
+			.login(NET_ID, PASSWORD)
+			.toCoursesView()
+			.listMyProjects()
+			.get(0).click()
+			.toContributorsView();
+
+		List<Contributor> contributors = view.listContributors();
+		assertEquals(2, contributors.size());
+
+		for(int i = 0, s = 2; i < s; i++) {
+			Contributor assignment = contributors.get(i);
+			if(i==0){
+				assertEquals(assignment.getNetID(), "student1");
+				assertEquals(assignment.getName(), "Student One");
+				assertEquals(assignment.getEmail(), "student-1@student.tudelft.nl");
+			}
+			if(i==1){
+				assertEquals(assignment.getNetID(), "student2");
+				assertEquals(assignment.getName(), "Student Two");
+				assertEquals(assignment.getEmail(), "student-2@student.tudelft.nl");
+			}
+		}
 	}
 
 }

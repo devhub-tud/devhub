61aeaa508fdc43bbf5209731f7eb467a199ee112
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2016-05-04 11:08:28 +0200

    Base setup to clone repository and create a branch

---
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/ProjectPullTest.java
@@ -0,0 +1,84 @@
+package nl.tudelft.ewi.devhub.webtests;
+
+import com.google.common.io.Files;
+import nl.tudelft.ewi.devhub.server.database.controllers.Groups;
+import nl.tudelft.ewi.devhub.server.database.controllers.Users;
+import nl.tudelft.ewi.devhub.server.database.entities.Group;
+import nl.tudelft.ewi.devhub.server.database.entities.GroupRepository;
+import nl.tudelft.ewi.devhub.server.database.entities.User;
+import nl.tudelft.ewi.devhub.webtests.utils.WebTest;
+import nl.tudelft.ewi.git.models.DetailedCommitModel;
+import nl.tudelft.ewi.git.web.api.BranchApi;
+import nl.tudelft.ewi.git.web.api.CommitApi;
+import nl.tudelft.ewi.git.web.api.RepositoriesApi;
+import nl.tudelft.ewi.git.web.api.RepositoryApi;
+import nl.tudelft.ewi.gitolite.repositories.RepositoriesManager;
+import nl.tudelft.ewi.gitolite.repositories.Repository;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import javax.inject.Inject;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class ProjectPullTest extends WebTest {
+
+	@Inject Users users;
+	@Inject Groups groups;
+	@Inject RepositoriesApi repositoriesApi;
+	@Inject RepositoriesManager repositoriesManager;
+
+	@Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	private User user;
+	private Group group;
+	private GroupRepository groupRepository;
+	private RepositoryApi repositoryApi;
+	private BranchApi masterApi;
+	private CommitApi commitApi;
+	private DetailedCommitModel commitModel;
+
+	@Before
+	public void prepareInitialCommit() {
+		user = users.findByNetId(NET_ID);
+		group = groups.listFor(user).get(0);
+		groupRepository = group.getRepository();
+		repositoryApi = repositoriesApi.getRepository(groupRepository.getRepositoryName());
+		masterApi = repositoryApi.getBranch("master");
+		commitApi = masterApi.getCommit();
+		commitModel = commitApi.get();
+	}
+
+	public void createRepository() throws URISyntaxException, GitAPIException, IOException {
+		Repository repository = repositoriesManager.getRepository(new URI(groupRepository.getRepositoryName() + ".git/"));
+
+		Git git = Git.cloneRepository()
+			.setBare(false)
+			.setDirectory(temporaryFolder.getRoot())
+			.setRemote(repository.getPath().toString())
+			.call();
+
+		git.checkout().setName("my-super-branch").call();
+		Files.write("Initial content".getBytes(), new File(temporaryFolder.getRoot(), "my-file.txt"));
+		git.add().addFilepattern("my-file.txt").call();
+		git.commit().setMessage("Adding my-file.txt").setAuthor(user.getName(), user.getEmail()).call();
+		git.push().setRemote("origin/my-super-branch").call();
+	}
+
+	@Test
+	public void testListCommits() {
+		System.out.println("");
+	}
+
+}

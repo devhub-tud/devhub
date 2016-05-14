0f51d937b5b3b3ef225780db88a9f58acf1d49f7
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-05-14 18:22:04 +0200

    Add tests to check if clicking next works with a currect student id
    If clicking next doesn't work then the test will not be able to find the group members table.

--- src/test/java/nl/tudelft/ewi/devhub/webtests/GroupCourseEnrollTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/GroupCourseEnrollTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.ewi.devhub.webtests;
 
+import nl.tudelft.ewi.devhub.server.database.entities.User;
 import nl.tudelft.ewi.devhub.webtests.utils.WebTest;
 import nl.tudelft.ewi.devhub.webtests.views.CoursesView;
+import nl.tudelft.ewi.devhub.webtests.views.GroupEnrollView;
 import org.junit.Test;
 
 import java.util.List;
@@ -13,6 +15,9 @@ public class GroupCourseEnrollTest extends WebTest {
     // It is imperative that this student is NOT enrolled in a group
     private static final String NET_ID = "student5";
     private static final String PASSWORD = "student5";
+    private static final String OTHER_STUDENT_ID = "student6";
+    private static final String STUDENT_NAME = "Student Five";
+    private static final String OTHER_STUDENT_NAME = "Student Six";
 
     /**
      * <h1>Scenario: Enrolling as a group</h1>
@@ -41,5 +46,43 @@ public class GroupCourseEnrollTest extends WebTest {
         assertEquals(1, courses.size());
 
         courses.get(0).clickEnroll();
+    }
+
+    /**
+     * <h1>Scenario: Enrolling as a group</h1>
+     *
+     * Given that:
+     * <ol>
+     *     <li>I am successfully logged in.</li>
+     *     <li>I am setting up a group for a new course.</li>
+     * </ol>
+     * When:
+     * <ol>
+     *     <li>I entered a correct student id for my partner.</li>
+     * </ol>
+     * Then:
+     * <ol>
+     *     <li>I can click on next to enroll.</li>
+     * </ol>
+     */
+    @Test
+    public void testCorrectStudentNumber() {
+        CoursesView view = openLoginScreen()
+                .login(NET_ID, PASSWORD)
+                .toCoursesView();
+
+        List<CoursesView.CourseOverview> courses = view.listAvailableCourses();
+        assertEquals(1, courses.size());
+
+        GroupEnrollView groupView = courses.get(0)
+                .clickEnroll()
+                .setMember2Field(OTHER_STUDENT_ID)
+                .clickNext();
+
+        List<User> groupMembers = groupView.groupMembers();
+        assertEquals(NET_ID, groupMembers.get(0).getNetId());
+        assertEquals(OTHER_STUDENT_ID, groupMembers.get(1).getNetId());
+        assertEquals(STUDENT_NAME, groupMembers.get(0).getName());
+        assertEquals(OTHER_STUDENT_NAME, groupMembers.get(1).getName());
     }
 }
--- src/test/java/nl/tudelft/ewi/devhub/webtests/views/GroupEnrollView.java
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/views/GroupEnrollView.java
@@ -1,10 +1,24 @@
 package nl.tudelft.ewi.devhub.webtests.views;
 
+import com.google.common.collect.Lists;
+import nl.tudelft.ewi.devhub.server.database.entities.User;
+import nl.tudelft.ewi.devhub.webtests.utils.Dom;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
 public class GroupEnrollView extends AuthenticatedView {
+
+    private static final By MEMBER2_FIELD = By.name("member-2");
+    private static final By NEXT_BUTTON = By.name("next");
+    private static final By GROUP_MEMBERS_LABEL = By.xpath("//label[starts-with(normalize-space(.), 'Group members')]");
+    private static final String TABLE_NETID_CLASSNAME = "truncate";
+
 
     public GroupEnrollView(WebDriver driver) {
         super(driver);
@@ -16,4 +30,57 @@ public class GroupEnrollView extends AuthenticatedView {
         assertTrue(currentPathStartsWith("/courses"));
         assertTrue(currentPathContains("/enroll"));
     }
+
+    /**
+     * This method fills in the given <code>student net-id</code> into the second member input box.
+     *
+     * @param studentNetId The student id to fill in.
+     * @return The current {@link GroupEnrollView}.
+     */
+    public GroupEnrollView setMember2Field(String studentNetId) {
+        invariant();
+        WebElement member2Field = getDriver().findElement(MEMBER2_FIELD);
+        member2Field.sendKeys(studentNetId);
+        return this;
+    }
+
+    /**
+     * This method clicks the next button.
+     *
+     * @return The current {@link GroupEnrollView}.
+     */
+    public GroupEnrollView clickNext() {
+        invariant();
+        WebElement next = getDriver().findElement(NEXT_BUTTON);
+        next.click();
+        return this;
+    }
+
+    public List<User> groupMembers() {
+        invariant();
+        WebElement label = getDriver().findElement(GROUP_MEMBERS_LABEL);
+        WebElement table = Dom.nextSibling(label, "table");
+        return listUsersFromTable(table);
+    }
+
+    private List<User> listUsersFromTable(WebElement table) {
+        List<WebElement> entries = table.findElements(By.tagName("td"));
+        if (entries.size() == 1) {
+            if (!Dom.isVisible(entries.get(0), By.tagName("a"))) {
+                return Lists.newArrayList();
+            }
+        }
+
+        List<User> users = new ArrayList<>();
+        for (WebElement entry : entries) {
+            WebElement studentId = entry.findElement(By.className(TABLE_NETID_CLASSNAME));
+            WebElement studentName = entry.findElement(By.tagName("b"));
+            User user = new User();
+            user.setName(studentName.getText());
+            user.setNetId(studentId.getText());
+            users.add(user);
+        }
+        return users;
+    }
+
 }

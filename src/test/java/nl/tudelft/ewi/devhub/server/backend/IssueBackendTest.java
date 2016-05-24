c9aec644b881b3e0c7dbd651a7e6305ba7737f34
ASZwaan <aronzwaan@solcon.nl>
2016-05-24 22:04:28 +0200

    Clean up IssueBackendTest

--- src/test/java/nl/tudelft/ewi/devhub/server/backend/IssueBackendTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/server/backend/IssueBackendTest.java
@@ -1,51 +1,38 @@
 package nl.tudelft.ewi.devhub.server.backend;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
-import javax.inject.Inject;
-
-import org.jukito.JukitoRunner;
-import org.jukito.UseModules;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import lombok.Getter;
 import nl.tudelft.ewi.devhub.server.database.controllers.CourseEditions;
 import nl.tudelft.ewi.devhub.server.database.controllers.Groups;
 import nl.tudelft.ewi.devhub.server.database.controllers.Issues;
-import nl.tudelft.ewi.devhub.server.database.controllers.TestDatabaseModule;
 import nl.tudelft.ewi.devhub.server.database.controllers.Users;
-import nl.tudelft.ewi.devhub.server.database.entities.User;
 import nl.tudelft.ewi.devhub.server.database.entities.issues.Issue;
-import nl.tudelft.ewi.git.web.api.RepositoriesApi;
 import nl.tudelft.ewi.git.web.api.RepositoryApi;
 
-@RunWith(JukitoRunner.class)
-@UseModules(TestDatabaseModule.class)
-public class IssueBackendTest extends PersistedBackendTest {
+@RunWith(MockitoJUnitRunner.class)
+public class IssueBackendTest extends BackendTest {
 
-	private static RepositoriesApi repositoriesApi = mock(RepositoriesApi.class);
-
-	@Inject @Getter private CourseEditions courses;
-	@Inject @Getter private Users users;
-	@Inject @Getter private Groups groups;
+	@Mock private CourseEditions courses;
+	@Mock private Users users;
+	@Mock private Groups groups;
 	
-	private Issues issues = mock(Issues.class);	
-	private IssueBackend issueBackend;	
+	@Mock private Issues issues;
+	@InjectMocks private IssueBackend issueBackend;	
+	
 	private Issue issue;
 	
-	private User user;
-	
+	@Mock
 	private RepositoryApi repositoryApi;
 	
 	@Before
 	public void setUp() throws Exception {
-		issueBackend = new IssueBackend(issues);
-		user = createUser();
-		repositoryApi = repositoriesApi.getRepository(createGroup(createCourseEdition(), user).getRepository().getRepositoryName());
-		
 		issue = new Issue();
 		
 	}

e3c35b1bac3957cc520a3be85102e8a8557f0569
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-05-24 22:18:29 +0200

    Add tooManyFiles test to LargeCommitWarningGenerator

--- src/test/java/nl/tudelft/ewi/devhub/server/backend/warnings/LargeCommitWarningGeneratorTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/server/backend/warnings/LargeCommitWarningGeneratorTest.java
@@ -1,8 +1,10 @@
 package nl.tudelft.ewi.devhub.server.backend.warnings;
 
+import com.google.common.collect.Sets;
 import nl.tudelft.ewi.devhub.server.database.Configurable;
 import nl.tudelft.ewi.devhub.server.database.entities.Commit;
 import nl.tudelft.ewi.devhub.server.database.entities.RepositoryEntity;
+import nl.tudelft.ewi.devhub.server.database.entities.warnings.LargeCommitWarning;
 import nl.tudelft.ewi.devhub.server.web.models.GitPush;
 import nl.tudelft.ewi.git.models.AbstractDiffModel;
 import nl.tudelft.ewi.git.models.DiffModel;
@@ -14,10 +16,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.OngoingStubbing;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,16 +32,18 @@ import static org.mockito.Mockito.when;
 public class LargeCommitWarningGeneratorTest {
 
     private LargeCommitWarningGenerator generator;
+    private LargeCommitWarning warning;
 
-    private static final String MAX_FILES_PROPERTY = "warnings.max-touched-files";
     private static final int MAX_AMOUNT_OF_FILES = 10;
+    private static final int MAX_AMOUNT_OF_LINES_TOUCHED = 500;
+    private static final String MAX_FILES_PROPERTY = "warnings.max-touched-files";
+    private static final String MAX_LINE_TOUCHED_PROPERTY = "warnings.max-line-edits";
     private static final String REPOSITORY_NAME = "John Cena";
     private static final String COMMIT_ID = "1";
 
     @Mock private RepositoriesApi repositoriesApi;
     @Mock private Commit commit;
     @Mock private RepositoryEntity configurable;
-    @Mock private List<AbstractDiffModel.DiffFile<AbstractDiffModel.DiffContext<AbstractDiffModel.DiffLine>>> diffFiles;
     @Mock private GitPush gitPush;
     @Mock private List<AbstractDiffModel.DiffFile<AbstractDiffModel.DiffContext<AbstractDiffModel.DiffLine>>> diffs;
     @Mock private AbstractCommitWarningGenerator abstractCommitWarningGenerator;
@@ -46,6 +54,7 @@ public class LargeCommitWarningGeneratorTest {
     @Before
     public void setUp() {
         generator = new LargeCommitWarningGenerator(repositoriesApi);
+        warning = new LargeCommitWarning();
         when(commit.getRepository()).thenReturn(configurable);
         when(commit.getRepository().getRepositoryName()).thenReturn(REPOSITORY_NAME);
         when(commit.getCommitId()).thenReturn(COMMIT_ID);
@@ -55,6 +64,7 @@ public class LargeCommitWarningGeneratorTest {
         when(commitApi.diff()).thenReturn(diffModel);
         when(diffModel.getDiffs()).thenReturn(diffs);
         when(repositoriesApi.getRepository(REPOSITORY_NAME)).thenReturn(repositoryApi);
+        when(commit.isMerge()).thenReturn(false);
     }
 
     @Test
@@ -62,5 +72,21 @@ public class LargeCommitWarningGeneratorTest {
         when(commit.isMerge()).thenReturn(true);
         Set empty = generator.generateWarnings(commit, gitPush);
         assertTrue(empty.isEmpty());
+    }
+
+    @Test
+    public void testTooManyFiles() {
+        when(diffs.size()).thenReturn(1);
+        when(configurable.getIntegerProperty(MAX_FILES_PROPERTY, MAX_AMOUNT_OF_FILES)).thenReturn(0);
+
+        warning.setCommit(commit);
+        Set testEquals = Sets.newHashSet(warning);
+        Set warnings = generator.generateWarnings(commit, gitPush);
+
+        verify(diffs).size();
+
+        assertFalse(warnings.isEmpty());
+        assertEquals(1, warnings.size());
+        assertEquals(testEquals, warnings);
     }
 }

bf220f35bb423ef58b8a5bdd85660aff8af9dc51
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-05-26 00:30:26 +0200

    Finished testing LargeCommitWarningGenerator
    It seems that there is a bug, it does NOT generate a warning on too many line changes.

--- src/test/java/nl/tudelft/ewi/devhub/server/backend/warnings/LargeCommitWarningGeneratorTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/server/backend/warnings/LargeCommitWarningGeneratorTest.java
@@ -1,6 +1,5 @@
 package nl.tudelft.ewi.devhub.server.backend.warnings;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -9,12 +8,10 @@ import nl.tudelft.ewi.devhub.server.database.entities.PrivateRepository;
 import nl.tudelft.ewi.devhub.server.database.entities.warnings.LargeCommitWarning;
 import nl.tudelft.ewi.devhub.server.web.models.GitPush;
 import nl.tudelft.ewi.git.models.AbstractDiffModel;
-import nl.tudelft.ewi.git.models.ChangeType;
 import nl.tudelft.ewi.git.models.DiffModel;
 import nl.tudelft.ewi.git.web.api.CommitApi;
 import nl.tudelft.ewi.git.web.api.RepositoriesApi;
 import nl.tudelft.ewi.git.web.api.RepositoryApi;
-import org.apache.commons.io.output.CloseShieldOutputStream;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,12 +19,12 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -39,9 +36,12 @@ public class LargeCommitWarningGeneratorTest {
     private PrivateRepository repository;
     private Commit commit;
     private DiffModel diffModel;
+    private AbstractDiffModel.DiffFile diffFile;
 
     private static final String REPOSITORY_NAME = "John Cena";
     private static final String COMMIT_ID = "1";
+    private static final int MAX_AMOUNT_OF_FILES = 10;
+    private static final int MAX_AMOUNT_OF_LINES_TOUCHED = 500;
 
     @Mock private RepositoriesApi repositoriesApi;
     @Mock private RepositoryApi repositoryApi;
@@ -66,21 +66,12 @@ public class LargeCommitWarningGeneratorTest {
         when(repositoryApi.getCommit(anyString())).thenReturn(commitApi);
         when(commitApi.diff()).thenReturn(diffModel);
 
-        /* I will convert this to a JSON resource */
-        AbstractDiffModel.DiffLine diffLine = new AbstractDiffModel.DiffLine();
-        diffLine.setNewLineNumber(1);
-        diffLine.setContent("Hiephoi");
-
-        AbstractDiffModel.DiffContext<AbstractDiffModel.DiffLine> diffContext = new AbstractDiffModel.DiffContext<>();
-        diffContext.setLines(Lists.newArrayList(diffLine));
-        AbstractDiffModel.DiffFile<AbstractDiffModel.DiffContext<AbstractDiffModel.DiffLine>> diffFile = new AbstractDiffModel.DiffFile<>();
-        diffFile.setContexts(Lists.newArrayList(diffContext));
-        diffFile.setType(ChangeType.ADD);
-        diffFile.setNewPath("/etc/apache2/hack-me.conf");
+        ObjectMapper mapper = new ObjectMapper();
+        diffFile = mapper.readValue(
+                LargeCommitWarningGeneratorTest.class.getResourceAsStream("/test-diff.json"),
+                AbstractDiffModel.DiffFile.class
+        );
         diffModel.setDiffs(Lists.newArrayList(diffFile));
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(new CloseShieldOutputStream(System.out), diffModel); // :-)
 
         /* Set up a warning, used to test if the generator returns an equal object */
         LargeCommitWarning warning = new LargeCommitWarning();
@@ -102,7 +93,12 @@ public class LargeCommitWarningGeneratorTest {
      * Confirms that the warning is generated when there are too many files committed.
      */
     @Test
-    public void testTooManyFiles() {
+    public void testTooManyFiles() throws IOException {
+        List<AbstractDiffModel.DiffFile<AbstractDiffModel.DiffContext<AbstractDiffModel.DiffLine>>> diffs = Lists.newArrayList();
+        for (int i = 0; i < MAX_AMOUNT_OF_FILES + 2; i++) {
+            diffs.add(diffFile);
+        }
+        diffModel.setDiffs(diffs);
         Set<LargeCommitWarning> warnings = generator.generateWarnings(commit, gitPush);
 
         assertEquals(1, warnings.size());
@@ -114,6 +110,12 @@ public class LargeCommitWarningGeneratorTest {
      */
     @Test
     public void testTooManyLineChanges() {
+        List<AbstractDiffModel.DiffLine> diffLines = Lists.newArrayList();
+        for (int i = 0; i < MAX_AMOUNT_OF_LINES_TOUCHED + 2; i++) {
+            AbstractDiffModel.DiffLine diffLine = new AbstractDiffModel.DiffLine(2*i, 2*i+1, String.valueOf(i));
+            diffLines.add(diffLine);
+        }
+        diffModel.getDiffs().get(0).getContexts().get(0).setLines(diffLines);
         Set<LargeCommitWarning> warnings = generator.generateWarnings(commit, gitPush);
 
         assertEquals(testEquals, warnings);

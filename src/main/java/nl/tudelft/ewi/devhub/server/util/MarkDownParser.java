e4880db283ecc99bb1c77eeffe5e1778db83a2b4
Douwe Koopmans <d.koopmans@student.tudelft.nl>
2016-06-03 18:12:42 +0200

    allow changing the markdown timeout and remove redundant null check

--- src/main/java/nl/tudelft/ewi/devhub/server/util/MarkDownParser.java
+++ src/main/java/nl/tudelft/ewi/devhub/server/util/MarkDownParser.java
@@ -2,14 +2,17 @@ package nl.tudelft.ewi.devhub.server.util;
 
 import lombok.NonNull;
 import lombok.Synchronized;
+import org.parboiled.errors.ParserRuntimeException;
 import org.pegdown.PegDownProcessor;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Douwe Koopmans on 1-6-16.
  */
 // TODO: 2-6-16 add preview panel when writing markdown
 public final class MarkDownParser {
-    public static final int timeout = 2000;
+    private static long timeout = 2000L;
 
     // initialising the processor takes a little bit, so it is smarter to reuse one initialisation
     // default parsing timeout is 2 seconds
@@ -23,18 +26,23 @@ public final class MarkDownParser {
      * raw markdown is returned instead
      * @throws NullPointerException when the given markdown string input is null
      */
-    public static String markdownToHtml(@NonNull final String md) throws NullPointerException{
-        final String generatedHtml = getProcessor().markdownToHtml(md);
-        if (generatedHtml == null) {
+    public static @NotNull String markdownToHtml(@NonNull final String md) throws NullPointerException {
+        try {
+            return getProcessor().markdownToHtml(md);
+        } catch (ParserRuntimeException ex) {
             return md;
         }
-
-        return generatedHtml;
     }
 
     // a bit extensive maybe, but this need to be thread-safe
     @Synchronized
     private static PegDownProcessor getProcessor() {
         return processor;
+    }
+
+    @Synchronized
+    public static void setProcessorTimeout(long newTimeout) {
+        timeout = newTimeout;
+        processor = new PegDownProcessor(timeout);
     }
 }

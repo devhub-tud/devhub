77c05083d57265c7142a371b6aa2d4f1667bbe90
Jurriaan Den Toonder <speedy4104@hotmail.com>
2016-06-08 10:52:10 +0200

    Add check for comments in DiffElement
    Default behaviour was to expect 3 columns, however when there is a comment this is only one.

--- src/test/java/nl/tudelft/ewi/devhub/webtests/DiffInCommitViewTest.java
+++ src/test/java/nl/tudelft/ewi/devhub/webtests/DiffInCommitViewTest.java
@@ -4,8 +4,9 @@ import nl.tudelft.ewi.devhub.webtests.utils.WebTest;
 import nl.tudelft.ewi.devhub.webtests.views.DiffInCommitView;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class CommentTest extends WebTest {
+public class DiffInCommitViewTest extends WebTest {
 
     @Test
     public void testCommentInline() {
@@ -20,7 +21,11 @@ public class CommentTest extends WebTest {
                 .get(0).click();
 
         commitView = commitView.postInlineComment(":grinning:");
-//        this.waitForCondition(5, commitView.listDiffs().get(0).getElement().findElement(By.cssSelector()));
+        this.waitForCondition(5, webDriver -> {
+            WebElement comment = getDriver().findElement(By.cssSelector("div.panel-comment"));
+            String commentText = comment.findElement(By.className("panel-body")).getText();
+            return commentText.charAt(0) == '\uD83D' && commentText.charAt(1) == '\uDE00';
+        });
 
         System.out.println("\n\n\n\n=============\n" + commitView.listDiffs());
     }
